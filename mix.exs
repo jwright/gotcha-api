b8fc234ff918aca05176607b187f09a2859788c7
Jamie Wright <jamie@brilliantfantastic.com>
2019-08-25 17:38:07 -0400

    Add ex_machina

--- mix.exs
+++ mix.exs
@@ -33,15 +33,16 @@ defmodule Gotcha.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:absinthe, "~> 1.4.0"},
+      {:ecto_sql, "~> 3.1"},
+      {:ex_machina, "~> 2.3", only: :test},
+      {:gettext, "~> 0.11"},
+      {:jason, "~> 1.0"},
       {:phoenix, "~> 1.4.9"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
-      {:ecto_sql, "~> 3.1"},
-      {:postgrex, ">= 0.0.0"},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:absinthe, "~> 1.4.0"},
+      {:postgrex, ">= 0.0.0"}
     ]
   end
 
